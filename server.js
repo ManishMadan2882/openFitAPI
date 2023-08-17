7362fee44b41addd121269042db8fad843eb9550
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-08-18 01:02:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(express.json())
 app.use('/api/v1',PromptRouter);
 app.use('/api/v1',mLRouter);
 
+app.get("/",(req,res,next)=>{
+    res.status(200).json({
+        success:true,
+        message:"Working"
+    })
+})
 
 app.use(errorMiddleware);
 app.listen(port,()=>{
