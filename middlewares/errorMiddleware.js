14a52e49a0c39cb0c2d422a56af1e5a917fbb2e9
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-08-18 11:25:53 +0530

    Update errorMiddleware.js

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,9 +2,9 @@ const errorMiddleware = (err,req,res,next)=>{
     err.message = err.message || "Internal Server Error";
     err.statusCode = err.statusCode || 500;
 
-    res.status(statusCode).json({
+    res.status(err.statusCode).json({
         success:false,
-        error:message
+        error:err.message
     })
 }
 
